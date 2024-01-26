2d1cc8747f1602799c37f982942e5cbdff4e6725
MathsLima <matheuslima13@hotmail.com>
2024-01-26 15:19:07 -0300

    update insert data on mysql

--- main.py
+++ main.py
@@ -42,13 +42,13 @@ class App:
     def tela_inicial(self):
         self.muda_janela()
 
-        label_tela_inicial = tk.Label(self.container_principal, text="Sistema de Cadastro de Miniaturas", font=('Calibri', 13), bg=bg, fg=letras )
+        label_tela_inicial = tk.Label(self.container_principal, text="Sistema de Cadastro de Miniaturas", font=('Calibri', 13), bg=bg, fg=letras)
         label_tela_inicial.place(width=250, height=40, x=25, y=10)
 
-        botao_pagina_1 = tk.Button(self.container_principal, text="Consultar Coleção", command=self.pagina_1)
+        botao_pagina_1 = tk.Button(self.container_principal, text="Cadastrar Miniatura", command=self.pagina_1)
         botao_pagina_1.place(width=200, height=40, x=50, y=60)
 
-        botao_pagina_2 = tk.Button(self.container_principal, text="Cadastrar Miniatura", command=self.pagina_2)
+        botao_pagina_2 = tk.Button(self.container_principal, text="Consultar Coleção", command=self.pagina_2)
         botao_pagina_2.place(width=200, height=40, x=50, y=110)
 
         botao_pagina_3 = tk.Button(self.container_principal,text="Excluir Miniatura", command=self.pagina_3)
@@ -57,28 +57,68 @@ class App:
         #deixa com o menu com o tamanho inicial
         self.root.geometry(self.tamanho_inicial)
 
+
+    #pagina de cadastro de miniaturas
     def pagina_1(self):
         self.muda_janela()
-        self.root.geometry("600x400")
-        
-        botao_voltar = tk.Button(self.container_principal, text=" ⬅ Voltar", command=self.tela_inicial)
-        botao_voltar.pack(pady=10)
+        self.root.geometry("485x350")
+
+        label_titulo = tk.Label(self.container_principal, text="Cadastrar Miniaturas", font=('Calibri', 18), bg=bg, fg=letras)
+        label_titulo.place(width=300, height=25, x=100, y=15)
+
+        botao_voltar = tk.Button(self.container_principal, text="⬅ Voltar", command=self.tela_inicial)
+        botao_voltar.place(width=65, height=25, x=400, y=15)
+
+        label_modelo = tk.Label(self.container_principal, text="Modelo:", font=('Calibri', 15), bg=bg, fg=letras)
+        label_modelo.place(width=75, height=30, x=25, y=75)
+
+        input_modelo = tk.Entry(self.container_principal)
+        input_modelo.place(width=250, height=30, x=125, y=75)
+
+        label_cor = tk.Label(self.container_principal, text="Cor:", font=('Calibri', 15), bg=bg, fg=letras)
+        label_cor.place(width=75, height=30, x=8, y=125)
+
+        input_cor = tk.Entry(self.container_principal)
+        input_cor.place(width=250, height=30, x=125, y=125)
+
+        label_marca = tk.Label(self.container_principal, text="Marca:", font=('Calibri', 15), bg=bg, fg=letras)
+        label_marca.place(width=75, height=30, x=17, y=175)
+
+        input_marca = tk.Entry(self.container_principal)
+        input_marca.place(width=250, height=30, x=125, y=175)
+
+        label_colecao = tk.Label(self.container_principal, text="Coleção:", font=('Calibri', 15), bg=bg, fg=letras)
+        label_colecao.place(width=75, height=30, x=22, y=225)
+
+        input_colecao = tk.Entry(self.container_principal)
+        input_colecao.place(width=250, height=30, x=125, y=225)
+
+        adicionar = tk.Button(self.container_principal, text="Adicionar na coleção", 
+                              font=('Calibri', 15),
+                              command=lambda: self.adicionar_miniatura(input_modelo.get(), input_cor.get(), input_marca.get(), input_colecao.get()))
+        adicionar.place(width=200, height=30, x=150, y=280)
+
 
     def pagina_2(self):
         self.muda_janela()
         self.root.geometry("600x400")
         
-        botao_voltar = tk.Button(self.container_principal, text="Voltar a Tela Inicial", command=self.tela_inicial)
-        botao_voltar.pack(pady=20)
+        botao_voltar = tk.Button(self.container_principal, text="⬅ Voltar", command=self.tela_inicial)
+        botao_voltar.place(width=75, height=25, x=510, y=15)
 
     def pagina_3(self):
         self.muda_janela()
         self.root.geometry("600x400")
         
+        botao_voltar = tk.Button(self.container_principal, text="⬅ Voltar", command=self.tela_inicial)
+        botao_voltar.place(width=75, height=25, x=510, y=15)
 
-        botao_voltar = tk.Button(self.container_principal, text="Voltar a Tela Inicial", command=self.tela_inicial)
-        botao_voltar.pack(pady=30)
-
+    
+    #funcao para adicionar miniaturas no banco de dados
+    def adicionar_miniatura(self, modelo, cor, marca, colecao):
+        comando = f'INSERT INTO minis (modelo, cor, marca, colecao) VALUES ("{modelo}", "{cor}", "{marca}", "{colecao}")'
+        cursor.execute(comando)
+        conexao.commit()
 
 #inicia a aplicacao
 root = tk.Tk()
